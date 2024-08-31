7e4ebd5c5e11619aea7874d3c2a96376fc43c743
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-08-31 21:19:34 +0500

    Geneation of random number

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,38 +1,22 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Die from './components/Die';
 
 function App() {
-	/**
-	 * Challenge:
-	 *
-	 * - Create a Die component that takes a `value` prop
-	 * - Render 10 instances of the Die component (manually)
-	 *      - Provide a number between 1-6 for the value on each
-	 *        for now
-	 * - Style the <main> and <Die> components
-	 *   to look like they do in the slide
-	 *      - Hints: Create a container to hold the 10 instances
-	 *        of the Die component, and use CSS Grid to lay them
-	 *        out evenly in 2 rows of 5 columns
-	 *      - Use flexbox on main to center the dice container
-	 *        in the center of the page
-	 */
+	const [diceValue, setDiceValue] = React.useState([]);
+
+	function allNewDice() {
+		for (let i = 0; i < 10; i++) {
+			diceValue.push(Math.floor(Math.random() * 6) + 1);
+		}
+	}
+	allNewDice();
+	const diceElements = diceValue.map(die =><Die value={die} />);
 	return (
 		<main className='container'>
-      <div className='dice-container'>
-			<Die value ='1'/>
-			<Die value ='1'/>
-			<Die value ='1'/>
-			<Die value ='1'/>
-			<Die value ='1'/>
-      <Die value ='1'/>
-			<Die value ='1'/>
-			<Die value ='1'/>
-			<Die value ='1'/>
-			<Die value ='1'/>
-
-      </div>
+			<div className='dice-container'>
+    {diceElements}
+			</div>
 		</main>
 	);
 }
