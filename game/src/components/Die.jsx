e06d08ac1960a69a6439259d55863a714eb92750
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-08-31 20:59:56 +0500

    framing the site making the foundation

---
+++ game/src/components/Die.jsx
@@ -0,0 +1,14 @@
+
+import { useState } from 'react'
+
+export default function Die(props) {
+
+
+return ( 
+  <div className='die-face' >
+    <h2 className='dice' >{props.value}</h2>
+    </div>
+)
+
+}
+
