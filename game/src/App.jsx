ac1ad5a1062b31d203a3ad031f4269f718c38719
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-08-31 22:50:48 +0500

    held feature of the game

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -3,54 +3,53 @@ import './App.css';
 import Die from './components/Die';
 import { nanoid } from 'nanoid';
 function App() {
-/**
- * Challenge: Create a function `holdDice` that takes
- * `id` as a parameter. For now, just have the function
- * console.log(id).
- * 
- * Then, figure out how to pass that function down to each
- * instance of the Die component so when each one is clicked,
- * it logs its own unique ID property. (Hint: there's more
- * than one way to make that work, so just choose whichever
- * you want)
- * 
- */
 	const [dice, setDice] = React.useState(allNewDice());
+	function generateNewDie() {
+		return {
+			value: Math.floor(Math.random() * 6),
+			isHeld: false,
+			id: nanoid(),
+		};
+	}
+
 	function allNewDice() {
 		const newDice = [];
 		for (let i = 0; i < 10; i++) {
-			newDice.push({
-				value: Math.ceil(Math.random() * 6),
-				isHeld: true,
-				id: nanoid(),
-			});
+			newDice.push(generateNewDie());
 		}
 		return newDice;
 	}
 
 	function rollDice() {
-		setDice(allNewDice());
+		setDice((oldDice) =>
+			oldDice.map((die) => {
+				return die.isHeld ? die : generateNewDie();
+			})
+		);
 	}
-  
-  function holdDice(id) {
-    setDice(oldDice => oldDice.map(die => {
-        return die.id === id ? 
-            {...die, isHeld: !die.isHeld} :
-            die
-    }))
-}
 
+	function holdDice(id) {
+		setDice((oldDice) =>
+			oldDice.map((die) => {
+				return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+			})
+		);
+	}
 	const diceElements = dice.map((die) => (
-		<Die 
-    key={die.id} 
-    value={die.value}
-    isHeld={die.isHeld}
-    holdDice={()=>holdDice(die.id)}
-    />
+		<Die
+			key={die.id}
+			value={die.value}
+			isHeld={die.isHeld}
+			holdDice={() => holdDice(die.id)}
+		/>
 	));
 
 	return (
-		<main className='container'>
+		<main>
+			<h1 className='title'>TENZIES</h1>
+			<p className='instruction'>
+      Roll ten dice and keep rolling until all dice match the same number. Try to finish as fast as you can!
+			</p>
 			<div className='dice-container'>{diceElements}</div>
 			<button onClick={rollDice} className='roll-dice'>
 				Roll
